443956bb55b1f252c152d47aebe9de5dd4518c51
Robin Mitra <robinmitra1@gmail.com>
2019-04-03 16:58:04 +0100

    Extract humanisation of disk usage to a separate module.

--- cmd/analyse.go
+++ cmd/analyse.go
@@ -3,18 +3,12 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"github.com/robinmitra/forest/disk"
 	"github.com/spf13/cobra"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
-)
-
-const (
-	B  = 1
-	KB = 1024 * B
-	MB = 1024 * KB
-	GB = 1024 * MB
 )
 
 type summary struct {
@@ -38,25 +32,11 @@ type analysis struct {
 	diskUsage   int64
 }
 
-func humanise(bytes int64) string {
-	if bytes < GB {
-		if bytes < MB {
-			if bytes < KB {
-				return fmt.Sprintf("%i B", bytes)
-			}
-			return fmt.Sprintf("%.2f KB", float64(bytes)/float64(KB))
-		}
-		return fmt.Sprintf("%.2f MB", float64(bytes)/float64(MB))
-	} else {
-		return fmt.Sprintf("%.2f GB", float64(bytes)/float64(GB))
-	}
-}
-
 func (s summary) print() {
 	fmt.Println("\nSummary:")
 	fmt.Println("Files:", s.numFiles)
 	fmt.Println("Directories:", s.numDirectories)
-	fmt.Println("Disk usage:", humanise(s.diskUsage))
+	fmt.Println("Disk usage:", disk.Humanise(s.diskUsage))
 }
 
 var cmdAnalyse = &cobra.Command{
