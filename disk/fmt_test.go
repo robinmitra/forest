80b3d3942213aeda1f6a686fba2d1719ae510c4d
Robin Mitra <robinmitra1@gmail.com>
2019-04-06 20:12:44 +0100

    Optimise `TestHumanise()` to use sub-tests for better failure handling, and run these in parallel.

--- disk/fmt_test.go
+++ disk/fmt_test.go
@@ -1,32 +1,36 @@
 package disk
 
 import (
+	"fmt"
 	"testing"
 )
 
-var humaniseExamples = []struct {
-	in  int64
-	out string
-}{
-	{100, "100 B"},
-	{1024, "1.00 KB"},
-	{5000, "4.88 KB"},
-	{1024 * 1024, "1.00 MB"},
-	{5000000, "4.77 MB"},
-	{1024 * 1024 * 1024, "1.00 GB"},
-	{5000000000, "4.66 GB"},
-}
-
 func TestHumanise(t *testing.T) {
-	for _, example := range humaniseExamples {
-		res := Humanise(example.in)
-		if res != example.out {
-			t.Errorf(
-				"Expected humanisation of %d bytes to be %s, found %s",
-				example.in,
-				example.out,
-				res,
-			)
-		}
+	testCases := []struct {
+		in  int64
+		out string
+	}{
+		{100, "100 B"},
+		{1024, "1.00 KB"},
+		{5000, "4.88 KB"},
+		{1024 * 1024, "1.00 MB"},
+		{5000000, "4.77 MB"},
+		{1024 * 1024 * 1024, "1.00 GB"},
+		{5000000000, "4.66 GB"},
+	}
+	for _, tc := range testCases {
+		// Bind the current test case as otherwise `tc` will end up referring to the last one.
+		tc := tc
+		t.Run(fmt.Sprintf("Humanise %d bytes", tc.in), func(t *testing.T) {
+			t.Parallel()
+			if res := Humanise(tc.in); res != tc.out {
+				t.Fatalf(
+					"Expected humanisation of %d bytes to be %s, found %s",
+					tc.in,
+					tc.out,
+					res,
+				)
+			}
+		})
 	}
 }
