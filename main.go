a3fae7049c398a7b44b5706e1cd303fb596694ab
Robin Mitra <robinmitra1@gmail.com>
2019-04-11 19:07:00 +0100

    Move default logger config to `main.go`.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/robinmitra/forest/cmd"
+import (
+	"github.com/robinmitra/forest/cmd"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	cmd.Execute()
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	log.SetLevel(log.WarnLevel) // Only log the warning severity or above.
 }
